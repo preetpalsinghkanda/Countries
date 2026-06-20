ffd5eaeb8a8000070d92d5be59d7a8893e1cff5a
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-06-20 11:02:12 +0530

    added input or select option

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMagnifyingGlass,
+  faChevronDown,
+  faCheck
+} from "@fortawesome/free-solid-svg-icons";
+import { Listbox, Transition } from "@headlessui/react";
 
 const Hero = () => {
+  const regions = [
+    // "Filter by Region",
+    "Africa",
+    "America",
+    "Asia",
+    "Europe",
+    "Oceania",
+  ];
+
+  const [selected, setSelected] = useState("Filter by Region");
+
   return (
-    <div className=" px-20 my-10">
-
-
-      <div className="border rounded-md max-w-lg w-full">
+    <div className=" px-20 flex bg-[#fcfcfc] justify-between py-10">
+      <div className="shadow-[0_1px_8px_rgba(0,0,0,0.10)] bg-white rounded-md max-w-lg w-full">
         <div className=" px-6 flex gap-5 items-center py-3">
           <FontAwesomeIcon className="text-gray-500" icon={faMagnifyingGlass} />
-          <input type="text" placeholder="Search for a country..." className="border-0 w-full outline-hidden" />
+          <input
+            type="text"
+            placeholder="Search for a country..."
+            className="border-0 w-full outline-hidden"
+          />
         </div>
       </div>
 
-      <div className="border ">
+      <div className="w-52 ">
+        <Listbox value={selected} onChange={setSelected}>
+          <div className="relative">
+            <Listbox.Button className="shadow-[0_1px_8px_rgba(0,0,0,0.10)] flex w-full justify-between items-center outline-0 rounded-md  gap-8 px-4 py-3">
+              <span className="whitespace-nowrap">{selected}</span>
+              <span className="items-center">
+                <FontAwesomeIcon icon={faChevronDown} />
+              </span>
+            </Listbox.Button>
 
+            <Listbox.Options className="absolute w-full shadow-[0_1px_8px_rgba(0,0,0,0.10)] bg-white outline-hidden mt-2 px-4 rounded-md  py-3">
+              {regions.map((region) => (
+                <Listbox.Option className="cursor-pointer whitespace-nowrap" key={region} value={region}>
+                  {region}
+                   
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </div>
+        </Listbox>
       </div>
     </div>
   );
